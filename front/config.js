4f7790a04e12bea670e60353e329318117fa8463
jacobyc <jacobyc@spotv.net>
2024-11-29 14:43:36 +0900

    [fix] 로컬환경 운영환경 proxy 설정 수정

--- front/config.js
+++ front/config.js
@@ -1,3 +1,3 @@
 
 //export const PROXY = window.location.hostname === 'localhost' ? 'http://localhost:3000' : '/proxy';
-export const PROXY = 'http://211.118.142.83:3000';
+export const PROXY = window.location.protocol === 'https' ? '/proxy' : 'http://211.118.142.83:3000';
